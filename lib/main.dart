8a574daf197702776e143dcadf642b6200acae6f
Suven-p <pandeysuven@gmail.com>
2022-12-31 11:32:44 +0545

    Make Hacker Profile default

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'ui/landing_page.dart';
+import 'ui/hacker_profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LandingPage());
+    return const MaterialApp(home: HackerProfile(name: "Nevus2"));
   }
 }
