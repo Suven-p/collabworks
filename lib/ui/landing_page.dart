254f6d561975f05c157179ded6bf08903262e260
Suven-p <pandeysuven@gmail.com>
2022-12-31 08:01:31 +0545

    Fix logo width to 60%

--- lib/ui/landing_page.dart
+++ lib/ui/landing_page.dart
@@ -7,6 +7,8 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     void signUp() {
       Navigator.push(
         context,
@@ -32,14 +34,25 @@ class LandingPage extends StatelessWidget {
           children: <Widget>[
             Container(
                 padding: const EdgeInsets.only(left: 40, top: 60),
-                child: const Text("Welcome to\nColabworks",
+                child: const Text("Welcome to\nColabWorks",
                     style: TextStyle(
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
                       // color: Color.fromARGB(255, 244, 105, 94),
                     ))),
-            Image.network(
-                "https://cdn.discordapp.com/attachments/1057264312508481629/1058409871277555723/22500901072__1_-removebg-preview.png"),
+            Container(
+              width: size.width,
+              height: size.height * 0.34,
+              alignment: Alignment.center,
+              child: Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.network(
+                        "https://cdn.discordapp.com/attachments/1057264312508481629/1058409871277555723/22500901072__1_-removebg-preview.png",
+                        width: size.width * 0.6)
+                  ]),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
